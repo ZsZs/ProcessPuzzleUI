5e538575207049420fb20ca59b5a7a4237d6335b
ZsZs <zsolt.zsuffa@gmail.com>
2012-01-21 18:29:40 +0100

    Delete JsUnit from it's previous folder.

--- Implementation/WebTier/WebContent/JavaScript/Source/JsUnit/app/JsTraceMessage.js
+++
@@ -1,7 +0,0 @@
-var JsTraceMessage = new Class( {
-   initialize : function( message, value, traceLevel ) {
-      this.message = message;
-      this.value = value;
-      this.traceLevel = traceLevel;
-   },
-});
