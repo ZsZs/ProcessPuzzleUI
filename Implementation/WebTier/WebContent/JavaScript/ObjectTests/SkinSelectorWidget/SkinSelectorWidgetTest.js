96d1a80c93799f71fa650d1306baec99be8982ca
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2012-02-08 08:30:12 +0100

    Extract tests into a separate test class.

--- Implementation/WebTier/WebContent/JavaScript/ObjectTests/SkinSelectorWidget/SkinSelectorWidgetTest.js
+++ Implementation/WebTier/WebContent/JavaScript/ObjectTests/SkinSelectorWidget/SkinSelectorWidgetTest.js
@@ -1,63 +1,103 @@
 var SkinSelectorWidgetTest = new Class( {
    Implements : [Events, JsTestClass, Options],
-   Binds : ['onFailure', 'onSuccess'],
+   Binds : ['onSkinChangeCallback'],
 
    options : {
-      testMethods : 
-         [{ method : 'synchronTest', isAsynchron : false },
-          { method : 'asynchronTestOne', isAsynchron : true }, 
-          { method : 'asynchronTestTwo', isAsynchron : true }]
+      testMethods : [
+          { method : 'initialize_whenConstructorArgumentsAreGiven', isAsynchron : false },
+          { method : 'initialize_whenConstructorArgumentsAreMissing_usesWebUIController', isAsynchron : false },
+          { method : 'configure_CreatesSelectElementWithOptions', isAsynchron : false },
+          { method : 'onSelection_NotifiesMessageBusSubscribers', isAsynchron : false }]
    },
 
    constants : {
+      CONFIGURATION_URI : "../WebUIController/SampleConfiguration.xml",
+      ELEMENT_SELECTOR : "SkinSelector",
+      LANGUAGE : "hu",
+      SKIN_ONE_NAME : "MochaUI",
+      SKIN_ONE_PATH : "../DesktopConfigurator/Skins/MochaUI",
+      SKIN_TWO_NAME : "ProcessPuzzle",
+      SKIN_TWO_PATH : "../DesktopConfigurator/Skins/ProcessPuzzle",
+      WIDGET_CONTAINER_ID : "SkinSelectorWidget",
    },
    
    initialize : function( options ) {
       this.setOptions( options );
+      this.callBackMessage;
+      this.callBackWasCalled = false;
+      this.componentStateManager;
+      this.skinSelector;
+      this.locale = new Locale({ language : this.constants.LANGUAGE });
+      this.messageBus;
+      this.webUIConfiguration;
+      this.webUIController;
+      this.webUILogger;
+      this.widgetContainerElement;
    },   
 
    beforeEachTest : function(){
+   	this.messageBus = new WebUIMessageBus();
+   	this.webUIConfiguration = new WebUIConfiguration( this.constants.CONFIGURATION_URI );
+   	this.webUILogger = new WebUILogger( this.webUIConfiguration );
+   	this.componentStateManager = new ComponentStateManager();
+   	this.resourceBundle = new XMLResourceBundle( this.webUIConfiguration );
+   	this.resourceBundle.load( this.locale );
+   	this.skinSelector = new SkinSelectorWidget({ widgetContainerId : this.constants.WIDGET_CONTAINER_ID }, this.resourceBundle, this.webUIConfiguration );
+   	this.widgetContainerElement = this.skinSelector.getContainerElement();
+
+   	this.callBackWasCalled = false;
    },
    
    afterEachTest : function (){
+   	this.skinSelector.destroy();
+      this.messageBus.tearDown();
+      this.callBackMessage = null;
    },
    
-   synchronTest : function() {
+   initialize_whenConstructorArgumentsAreGiven : function() {
+      assertEquals( $( this.constants.WIDGET_CONTAINER_ID ), this.skinSelector.getContainerElement() );
+      assertEquals( "Initialization determines available skins.", 2, this.skinSelector.getAvailableSkins().size() ); 
+      assertNotNull( this.skinSelector.getAvailableSkins().get( this.constants.SKIN_ONE_NAME ) );
+      assertNotNull( this.skinSelector.getAvailableSkins().get( this.constants.SKIN_TWO_NAME ) );
+   },
+   
+   initialize_whenConstructorArgumentsAreMissing_usesWebUIController : function() {
+      //SETUP:
+      var webUIController = new WebUIController({ configurationUri : "../WebUIController/SampleConfiguration.xml" });
+      
       //EXCERCISE:
+      var skinSelector = new SkinSelectorWidget();
       
       //VERIFY:
+      assertTrue( "When 'SkinSelector' is instantiated without arguments, 'WebUIController' is used as data source.", true );
+      assertEquals( webUIController.getResourceBundle(), skinSelector.getResourceBundle() );
+      assertEquals( webUIController.getWebUIConfiguration(), skinSelector.getWebUIConfiguration() );
+      assertEquals( 2, skinSelector.getAvailableSkins().size() );
+      
+      //TEARDOWN:
+      webUIController.destroy();
    },
    
-   asynchronTestOne : function() {
-      this.testCaseChain.chain(
-         function(){
-            //EXCERCISE:
-         }.bind( this ),
-         function(){
-            //VERIFY:
-            this.testMethodReady();
-         }.bind( this )
-      ).callChain();
+   configure_CreatesSelectElementWithOptions : function() {
+      this.skinSelector.construct();
+      
+      var selectElement = this.widgetContainerElement.getElementById( this.constants.ELEMENT_SELECTOR );
+      assertNotNull( selectElement );
+      assertEquals( "The number of options elements:", 3, selectElement.getChildren().length );
    },
    
-   asynchronTestOne : function() {
-      this.testCaseChain.chain(
-         function(){
-            //EXCERCISE:
-         }.bind( this ),
-         function(){
-            //VERIFY:
-            this.testMethodReady();
-         }.bind( this )
-      ).callChain();
+   onSelection_NotifiesMessageBusSubscribers : function() {
+      this.skinSelector.construct();
+      this.messageBus.subscribeToMessage( SkinChangedMessage, this.onSkinChangeCallback );
+      
+      var selectElement = this.widgetContainerElement.getElementById( this.constants.ELEMENT_SELECTOR );
+      selectElement.fireEvent( 'change' );
+   	  
+      assertTrue( this.callBackWasCalled );
    },
    
-   onSuccess : function(){
-      this.testCaseChain.callChain();
-   },
-   
-   onFailure : function( error ){
-      this.testCaseChain.callChain();
+   onSkinChangeCallback : function( message ) {
+      this.callBackWasCalled = true;
+      this.callBackMessage = message;
    }
-
 });
