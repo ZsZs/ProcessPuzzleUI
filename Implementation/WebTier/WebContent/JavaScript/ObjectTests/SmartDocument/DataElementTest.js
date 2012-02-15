b4ff0261053b44fba625638c94768835cc85cbda
ZsZs <zsolt.zsuffa@gmail.com>
2012-02-15 07:11:36 +0100

    Extract DataElementTest into it's own .js file.

---
+++ Implementation/WebTier/WebContent/JavaScript/ObjectTests/SmartDocument/DataElementTest.js
@@ -0,0 +1,173 @@
+var DataElementTest = new Class( {
+   Implements : [Events, JsTestClass, Options],
+   Binds : ['onConstructed', 'onConstructionError'],
+
+   options : {
+      testMethods : [
+          { method : 'initialize_setsState', isAsynchron : false },
+          { method : 'unmarshall_determinesElementProperties', isAsynchron : false },
+          { method : 'unmarshall_whenMaxOccuresLargetThanOne_determinesNumberOfDataElements', isAsynchron : false },
+          { method : 'unmarshall_whenMaxOccuresLargetThanOne_instantiatesSiblingElements', isAsynchron : false },
+          { method : 'construct_retrievesBindedData', isAsynchron : true }, 
+          { method : 'construct_addsClass', isAsynchron : true }, 
+          { method : 'construct_whenMaxOccuresLargetThanOne_createsMultipleElements', isAsynchron : true }, 
+          { method : 'destroy_whenUnmarshalled_resetsProperties', isAsynchron : false }, 
+          { method : 'destroy_whenConstructed_removesHtmlElement', isAsynchron : true }, 
+          { method : 'destroy_whenNotUnmarshalled_raisesException', isAsynchron : false }]
+   },
+
+   constants : {
+      WEBUI_CONFIGURATION_URI : "../SmartDocument/WebUIConfiguration.xml",
+      DOCUMENT_CONTAINER_ID : "SmartDocument",
+      DOCUMENT_CONTENT_URI : "../SmartDocument/UserProfile.xml",
+      DOCUMENT_DEFINITION_URI : "../SmartDocument/SmartDocumentDefinition.xml",
+      ELEMENT_DEFINITION_SELECTOR : "/smartDocumentDefinition/documentBody/compositeElement/dataElement[@id='currentUser']",
+      MULTIPLE_DATA_SELECTOR : "/smartDocumentDefinition/documentBody/compositeElement/compositeElement/dataElement[@id='userAddresses']"
+   },
+   
+   initialize : function( options ) {
+      this.setOptions( options );
+      
+      this.bundle;
+      this.documentContainerElement;
+      this.documentContentResource;
+      this.documentDefinition;
+      this.dataElement;
+      this.documentInternationalization;
+      this.webUIConfiguration;
+      this.webUILogger;      
+   },   
+
+   beforeEachTest : function(){
+      this.webUIConfiguration = new WebUIConfiguration( this.constants.WEBUI_CONFIGURATION_URI );
+      this.webUILogger = new WebUILogger( this.webUIConfiguration );
+      this.bundle = new XMLResourceBundle( this.webUIConfiguration );
+      this.bundle.load( new Locale({ language : "en" }) );
+      
+      this.documentContentResource = new XmlResource(  this.constants.DOCUMENT_CONTENT_URI, { nameSpaces : "xmlns:sd='http://www.processpuzzle.com/SmartDocument'" } );
+      this.documentDefinition = new XmlResource( this.constants.DOCUMENT_DEFINITION_URI, { nameSpaces : "xmlns:sd='http://www.processpuzzle.com/SmartDocument'" } );
+      this.dataElement = new DataElement( this.documentDefinition.selectNode( this.constants.ELEMENT_DEFINITION_SELECTOR ), this.bundle, null, { onConstructed : this.onConstructed, onConstructionError : this.onConstructionError } );
+      this.documentContainerElement = $( this.constants.DOCUMENT_CONTAINER_ID );
+   },
+   
+   afterEachTest : function (){
+      this.bundle.release();
+      this.bundle = null;
+      
+      if( this.dataElement.getState() > DocumentElement.States.INITIALIZED ) this.dataElement.destroy();
+      this.dataElement = null;
+      this.documentContainerElement = null;
+   },
+   
+   initialize_setsState : function() {
+      assertThat( this.dataElement, not( nil() ));
+      assertThat( this.dataElement.getDefinitionElement(), not( nil() ));
+      assertThat( this.dataElement.getState(), equalTo( DocumentElement.States.INITIALIZED ));
+   },
+   
+   unmarshall_determinesElementProperties : function() {
+      this.dataElement.unmarshall();
+      
+      assertThat( this.dataElement.getBind(), equalTo( this.documentDefinition.selectNodeText( this.constants.ELEMENT_DEFINITION_SELECTOR + "/@bind" )));
+      assertThat( this.dataElement.getSource(), equalTo( this.documentDefinition.selectNodeText( this.constants.ELEMENT_DEFINITION_SELECTOR + "/@source" ) ));
+      assertThat( this.dataElement.getMaxOccures(), equalTo( this.documentDefinition.selectNodeText( this.constants.ELEMENT_DEFINITION_SELECTOR + "/@maxOccures" ) ));
+      assertThat( this.dataElement.getMinOccures(), equalTo( this.documentDefinition.selectNodeText( this.constants.ELEMENT_DEFINITION_SELECTOR + "/@minOccures" ) ));
+   },
+   
+   unmarshall_whenMaxOccuresLargetThanOne_determinesNumberOfDataElements : function() {
+      this.dataElement = new DataElement( this.documentDefinition.selectNode( this.constants.MULTIPLE_DATA_SELECTOR ), this.bundle, null, { onConstructed : this.onConstructed, onConstructionError : this.onConstructionError } );
+      this.dataElement.unmarshall();
+
+      assertThat( this.dataElement.getDataElementsNumber(), equalTo( this.documentContentResource.selectNodes( "userProfile/addresses/address" ).length ));
+   },
+   
+   unmarshall_whenMaxOccuresLargetThanOne_instantiatesSiblingElements : function() {
+      this.dataElement = new DataElement( this.documentDefinition.selectNode( this.constants.MULTIPLE_DATA_SELECTOR ), this.bundle, null, { onConstructed : this.onConstructed, onConstructionError : this.onConstructionError } );
+      this.dataElement.unmarshall();
+      
+      assertThat( this.dataElement.getSiblings().size(), equalTo( this.documentContentResource.selectNodes( "userProfile/addresses/address" ).length -1 ));
+   },
+   
+   construct_retrievesBindedData : function() {
+      this.testCaseChain.chain(
+         function(){
+            this.dataElement.unmarshall();
+            this.dataElement.construct( this.documentContainerElement, "bottom" );
+         }.bind( this ),
+         function(){
+            assertThat( this.dataElement.getDataXml().xmlAsText, equalTo( this.documentContentResource.xmlAsText ));
+            assertThat( this.dataElement.getHtmlElement().get( 'text' ), equalTo( this.documentContentResource.selectNodeText( "/userProfile/userName" )));
+            this.testMethodReady();
+         }.bind( this )
+      ).callChain();
+   },
+   
+   construct_addsClass : function() {
+      this.testCaseChain.chain(
+         function(){
+            this.dataElement.unmarshall();
+            this.dataElement.construct( this.documentContainerElement, "bottom" );
+         }.bind( this ),
+         function(){
+            assertThat( this.dataElement.getHtmlElement().hasClass( DataElement.CLASS ), is( true ));
+            this.testMethodReady();
+         }.bind( this )
+      ).callChain();
+   },
+   
+   construct_whenMaxOccuresLargetThanOne_createsMultipleElements : function() {
+      this.testCaseChain.chain(
+         function(){
+            this.dataElement = new DataElement( this.documentDefinition.selectNode( this.constants.MULTIPLE_DATA_SELECTOR ), this.bundle, null, { onConstructed : this.onConstructed, onConstructionError : this.onConstructionError } );
+            this.dataElement.unmarshall();
+            this.dataElement.construct( this.documentContainerElement, "bottom" );
+         }.bind( this ),
+         function(){
+            assertThat( this.documentContainerElement.getChildren().length, equalTo( this.documentContentResource.selectNodes( "userProfile/addresses/address" ).length ));
+            this.testMethodReady();
+         }.bind( this )
+      ).callChain();
+   },
+   
+   destroy_whenUnmarshalled_resetsProperties : function() {
+      this.dataElement.unmarshall();
+      this.dataElement.destroy();
+
+      assertThat( this.dataElement.getBind(), nil() );
+      assertThat( this.dataElement.getMaxOccures(), nil() );
+      assertThat( this.dataElement.getMinOccures(), nil() );
+      assertThat( this.dataElement.getSource(), nil() );
+   },
+   
+   destroy_whenConstructed_removesHtmlElement : function() {
+      this.testCaseChain.chain(
+         function(){
+            this.dataElement.unmarshall();
+            this.dataElement.construct( this.documentContainerElement, "bottom" );
+         }.bind( this ),
+         function(){
+            assertThat( this.documentContainerElement.getChildren(), hasItem( this.dataElement.getHtmlElement() ));
+            this.dataElement.destroy();
+            assertThat( this.documentContainerElement.getChildren().length, equalTo( 0 ));
+            this.testMethodReady();
+         }.bind( this )
+      ).callChain();
+   },
+   
+   destroy_whenNotUnmarshalled_raisesException : function() {
+      try{
+         this.dataElement.destroy();
+         fail();
+      }catch( e ){
+         assertThat( instanceOf( e, UnconfiguredDocumentElementException ), is( true ));
+      }
+   },
+   
+   onConstructed : function(){
+      this.testCaseChain.callChain();
+   },
+
+   onConstructionError : function(){
+      this.testCaseChain.callChain();
+   }
+});
