2eed86db141ff775e6b69b3f82cf2f9fa08e8f35
ZsZs <zsolt.zsuffa@gmail.com>
2012-02-17 11:38:26 +0100

    //= require ../ResourceManager/DocumentResource.js

--- Implementation/WebTier/WebContent/JavaScript/Source/ResourceManager/DocumentScript.js
+++ Implementation/WebTier/WebContent/JavaScript/Source/ResourceManager/DocumentScript.js
@@ -25,6 +25,7 @@ You should have received a copy of the GNU General Public License along with thi
 */
 
 //= require_directory ../FundamentalTypes
+//= require ../ResourceManager/DocumentResource.js
 
 var DocumentScript = new Class({
    Extends: DocumentResource,
