ae22663baf228c27f6447eaf394c2a753042d7cc
ZsZs <zsolt.zsuffa@gmail.com>
2012-03-07 20:25:21 +0100

    Locale string can be separated by hyphen - too.

--- Implementation/WebTier/WebContent/JavaScript/Source/Internationalization/Locale.js
+++ Implementation/WebTier/WebContent/JavaScript/Source/Internationalization/Locale.js
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 var Locale = new Class({
    Implements: Options,
    options: {
-      delimiter : ",",
+      delimiter : ",-",
       language : "en",
       country : null,
       variant : null
