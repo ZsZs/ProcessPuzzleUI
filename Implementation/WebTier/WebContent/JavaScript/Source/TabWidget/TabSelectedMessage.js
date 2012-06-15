50dca61fc83cd67c6be466dabd0bdbeb40266db6
ZsZs <zsolt.zsuffa@gmail.com>
2012-06-15 07:53:53 +0200

    TabSelectedMessage handles documentVariables.

--- Implementation/WebTier/WebContent/JavaScript/Source/TabWidget/TabSelectedMessage.js
+++ Implementation/WebTier/WebContent/JavaScript/Source/TabWidget/TabSelectedMessage.js
@@ -1,20 +1,29 @@
 /*
-ProcessPuzzle User Interface
-Backend agnostic, desktop like configurable, browser font-end based on MochaUI.
-Copyright (C) 2011  Joe Kueser, Zsolt Zsuffa
+Name: 
+    - TabSelectedMessage
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Description: 
+    - Represents the event when a Tab of a TabWidget was selected.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Requires:
+    - WebUIMessage
+Provides:
+    - TabSelectedMessage
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Part of: ProcessPuzzle Browser UI, Back-end agnostic, desktop like, highly configurable, browser font-end, based on MochaUI and MooTools. 
+http://www.processpuzzle.com
+
+Authors: 
+    - Zsolt Zsuffa
+
+Copyright: (C) 2011 This program is free software: you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //= require_directory ../FundamentalTypes
@@ -29,6 +38,7 @@ var TabSelectedMessage = new Class({
       documentContentURI: null,
       documentType: AbstractDocument.Types.SMART,
       documentURI: null,
+      documentVariables: null,
       name: "TabSelectedMessage",
       tabId: null
    },
@@ -48,6 +58,7 @@ var TabSelectedMessage = new Class({
    getDocumentContentURI: function() { return this.options.documentContentURI; },
    getDocumentType: function() { return this.options.documentType; },
    getDocumentURI: function() { return this.options.documentURI; },
+   getDocumentVariables: function() { return this.options.documentVariables; },
    getId: function() { return this.options.tabId; }
 });
 
